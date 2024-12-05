d49262d19b59ea9773c3d9c05202149830b7e736
Ignacio Ballesteros <igbprogramming@gmail.com>
2024-12-04 23:58:49 -0500

    starting to work on the implementation of the file reader and data manipulation

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/igballesteros/weather-analyzer
+
+go 1.23.2
--- main.go
+++ main.go
@@ -1,7 +1,42 @@
 package main
 
-import ("fmt")
+import (
+	"encoding/csv"
+	"fmt"
+	"log"
+	"os"
+)
 
-func main () {
-	fmt.Println("hello world")
+func openFile(fileName string) (file *os.File, err error) {
+
+	return os.Open(fileName)
+}
+
+// func remove(records []string) []string {
+
+// 	return records
+// }
+
+func main() {
+
+	fileName := "export.csv"
+
+	file, err := openFile(fileName)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println("File opened")
+
+	// defer function runs before the function finishes
+	defer file.Close()
+
+	reader := csv.NewReader(file)
+
+	records, err := reader.ReadAll()
+
+	if err != nil {
+		fmt.Println("error reading contents")
+	}
+
 }
